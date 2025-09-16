7722a95becdb5c4cb0c733d43d37671061e0fa33
Alvaro Figueroa <alvaroafa1@gmail.com>
2025-09-15 21:35:40 -0300

    Configuraciones basicas
    BD
    Users
    Rest framework

---
+++ backend/users/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Aquí se crean los modelos personalizados de usuario si es necesario.
+
+class User(AbstractUser):
+    rut = models.CharField(max_lenght=12, unique=True, null=True, blank=True)
